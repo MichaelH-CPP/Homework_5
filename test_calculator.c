5d490936ebf157c216e42ebcd38e51719e6e4c50
MichaelHawara <mnhawara@cpp.edu>
2025-03-12 04:41:28 +0000

    Finished Subtract Tests

--- test_calculator.c
+++ test_calculator.c
@@ -75,6 +75,39 @@ void test_subtract_underflow(void)
     TEST_ASSERT_TRUE(result > 0);
 }
 
+void test_multiply_positive_numbers(void)
+{
+    TEST_ASSERT_EQUAL(15, multiply(3, 5));
+}
+
+void test_multiply_positive_and_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(-20, multiply(5, -4));
+}
+
+void test_multiply_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(12, multiply(-3, -4));
+}
+
+void test_multiply_zero(void)
+{
+    TEST_ASSERT_EQUAL(0, multiply(5, 0));
+    TEST_ASSERT_EQUAL(0, multiply(0, 0));
+}
+
+void test_multiply_overflow(void)
+{
+    int result = multiply(INT_MAX, 2);
+    TEST_ASSERT_TRUE(result < 0);
+}
+
+void test_multiply_underflow(void)
+{
+    int result = multiply(INT_MIN, 2);
+    TEST_ASSERT_TRUE(result > 0);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
@@ -93,6 +126,14 @@ int main(void)
     RUN_TEST(test_subtract_zero);
     RUN_TEST(test_subtract_overflow);
     RUN_TEST(test_subtract_underflow);
+
+    printf("Running Multiplying Tests: \n");
+    RUN_TEST(test_multiply_positive_numbers);
+    RUN_TEST(test_multiply_positive_and_negative_numbers);
+    RUN_TEST(test_multiply_negative_numbers);
+    RUN_TEST(test_multiply_zero);
+    RUN_TEST(test_multiply_overflow);
+    RUN_TEST(test_multiply_underflow);
 
     return UNITY_END();
 }
